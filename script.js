34ebbc380718cb379fe8a23ba53be2f82c186056
ahmed-gelemli <ahmed.gelemli@gmail.com>
2021-12-06 21:44:51 +0400

    Bug Fix

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function myFunction() {
+function navToggle() {
 	var x = document.getElementById("mobile_links");
 	if (x.style.height === '') {
 		x.style.height = "50vw";
 	} else {
 		x.style.height = '';
 	}
+}
+
+function scrollBottom() {
+	window.scrollTo(0,document.body.scrollHeight);
 }
